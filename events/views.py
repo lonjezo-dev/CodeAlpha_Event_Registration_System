141c4b0e0a19aa06e6669bc9895c5bcaf2f6dbec
Lonjezo-dev <samuelgondwe741@gmail.com>
2025-10-26 22:38:45 +0200

    making use of the generic views

--- events/views.py
+++ events/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render
 from .models import Event
+from django.views import generic
 
 # Create your views here.
-def index(request):
-    events = Event.objects.all()
-    # compute a boolean to avoid re-evaluating the queryset inside the template
-    no_events = not events.exists()
-    return render(request, 'events/index.html', {'events': events, 'no_events': no_events})
-
-def detail(request, event_id):
-    try:
-        event = Event.objects.get(pk=event_id)
-    except Event.DoesNotExist:
-        return render(request, 'events/detail.html', {'error_message': 'Event not found.'})
-    return render(request, 'events/detail.html', {'event': event})
+class IndexView(generic.ListView):
+    template_name = 'events/index.html'
+    context_object_name = 'events'
+    
+    def get_queryset(self):
+        return Event.objects.all()
+ 
+class DetailView(generic.DetailView):
+    model = Event
+    template_name = 'events/detail.html'
 
 def create_event(request): 
     if request.method == 'POST':
