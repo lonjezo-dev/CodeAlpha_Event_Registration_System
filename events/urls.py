17c7488dcde7ad6a9f467fb9cfc439dab741a5ba
Lonjezo-dev <samuelgondwe741@gmail.com>
2025-10-27 09:29:42 +0200

    create a style sheet

--- events/urls.py
+++ events/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('detail/<int:event_id>/', views.detail, name='detail'),
-    path('create/', views.create_event, name='create_event'),
 ]
