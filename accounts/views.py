265fd6dd0338fd1b7a704b411425d1df4fefd5fb
Lonjezo-dev <samuelgondwe741@gmail.com>
2025-10-29 12:08:31 +0200

    create  views for registering and login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login 
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register(request):
+       if request.method == 'POST':
+           form = CustomUserCreationForm(request.POST)
+           if form.is_valid():
+               user = form.save(commit=True)
+               user.save()
+               messages.success(request, 'Registration successful! Please check your email to activate your account.')
+               return redirect('login')  # Redirect to the login page
+       else:
+           form = CustomUserCreationForm()
+       return render(request, 'registration/register.html', {'form': form})
+
+
+def user_login(request):
+       if request.method == 'POST':
+           form = AuthenticationForm(data=request.POST)
+           if form.is_valid():
+               username = form.cleaned_data.get('username')
+               password = form.cleaned_data.get('password')
+               user = authenticate(username=username, password=password)
+               if user is not None:
+                   login(request, user)
+                   return redirect('index')  # Redirect to a home page or dashboard
+       else:
+           form = AuthenticationForm()
+       return render(request, 'registration/login.html', {'form': form})
+   
